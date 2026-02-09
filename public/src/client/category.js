69bf62ba8718e27228f7d79ea1ab8b16f31bb133
SShridhula <shridhulasri@gmail.com>
2026-02-09 17:07:47 +0000

    fixed format

--- public/src/client/category.js
+++ public/src/client/category.js
@@ -1,61 +1,70 @@
 'use strict';
 
 define('forum/category', [], function () {
-var Category = {};
+	var Category = {};
 
-Category.init = function () {
-itFilterTabs();
-};
+	Category.init = function () {
+		initFilterTabs();
+	};
 
-function initFilterTabs() {
-urlParams = new URLSearchParams(window.location.search);
-currentAnswered = urlParams.get('answered'); // 'true' | 'false' | null
-tAnswered);
+	function initFilterTabs() {
+		var urlParams = new URLSearchParams(window.location.search);
+		var currentAnswered = urlParams.get('answered'); // 'true' | 'false' | null
+		updateActiveFilter(currentAnswered);
 
-Filter').on('click.categoryFilter', function (e) {
-tDefault();
-$btn = $(this);
-resolvedAttr = $btn.attr('data-resolved');
-filterType;
-(typeof resolvedAttr !== 'undefined') {
-(resolvedAttr === '' || resolvedAttr === null) {
-pe = 'all';
-else if (resolvedAttr === 'false') {
-pe = 'unanswered';
-else if (resolvedAttr === 'true') {
-pe = 'answered';
-else {
-pe = 'all';
-else {
-pe = $btn.data('filter') || 'all';
-Filter(filterType);
-ction applyFilter(filterTypeOrResolved) {
-answeredValue = '';
-(filterTypeOrResolved === 'unanswered' || filterTypeOrResolved === 'false') {
-sweredValue = 'false';
-else if (filterTypeOrResolved === 'answered' || filterTypeOrResolved === 'true') {
-sweredValue = 'true';
-url = new URL(window.location.href);
-(answeredValue === '') {
-swered');
-else {
-swered', answeredValue);
-sweredValue === '' ? null : answeredValue);
-.go(url.pathname + url.search);
-}
+		$('.filter-tab').off('click.categoryFilter').on('click.categoryFilter', function (e) {
+			e.preventDefault();
+			var $btn = $(this);
+			var resolvedAttr = $btn.attr('data-resolved');
+			var filterType;
+			if (typeof resolvedAttr !== 'undefined') {
+				if (resolvedAttr === '' || resolvedAttr === null) {
+					filterType = 'all';
+				} else if (resolvedAttr === 'false') {
+					filterType = 'unanswered';
+				} else if (resolvedAttr === 'true') {
+					filterType = 'answered';
+				} else {
+					filterType = 'all';
+				}
+			} else {
+				filterType = $btn.data('filter') || 'all';
+			}
+			applyFilter(filterType);
+		});
+	}
 
-function updateActiveFilter(answeredParam) {
-btn-primary').addClass('btn-outline-secondary');
-activeFilter = 'all';
-(answeredParam === 'false') {
-= 'unanswered';
-else if (answeredParam === 'true') {
-= 'answered';
-byFilterSel = '.filter-tab[data-filter="' + activeFilter + '"]';
-resolvedVal = activeFilter === 'all' ? '' : (activeFilter === 'answered' ? 'true' : 'false');
-byResolvedSel = '.filter-tab[data-resolved="' + resolvedVal + '"]';
-FilterSel).add(byResolvedSel).addClass('active btn-primary').removeClass('btn-outline-secondary');
-}
+	function applyFilter(filterTypeOrResolved) {
+		var answeredValue = '';
+		if (filterTypeOrResolved === 'unanswered' || filterTypeOrResolved === 'false') {
+			answeredValue = 'false';
+		} else if (filterTypeOrResolved === 'answered' || filterTypeOrResolved === 'true') {
+			answeredValue = 'true';
+		}
 
-return Category;
+		var url = new URL(window.location.href);
+		if (answeredValue === '') {
+			url.searchParams.delete('answered');
+		} else {
+			url.searchParams.set('answered', answeredValue);
+		}
+		updateActiveFilter(answeredValue === '' ? null : answeredValue);
+		ajaxify.go(url.pathname + url.search);
+	}
+
+	function updateActiveFilter(answeredParam) {
+		$('.filter-tab').removeClass('active btn-primary').addClass('btn-outline-secondary');
+		var activeFilter = 'all';
+		if (answeredParam === 'false') {
+			activeFilter = 'unanswered';
+		} else if (answeredParam === 'true') {
+			activeFilter = 'answered';
+		}
+		var byFilterSel = '.filter-tab[data-filter="' + activeFilter + '"]';
+		var resolvedVal = activeFilter === 'all' ? '' : (activeFilter === 'answered' ? 'true' : 'false');
+		var byResolvedSel = '.filter-tab[data-resolved="' + resolvedVal + '"]';
+		$(byFilterSel).add(byResolvedSel).addClass('active btn-primary').removeClass('btn-outline-secondary');
+	}
+
+	return Category;
 });
